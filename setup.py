e25e52b043ee99cd0e8636da4631e9d43ede1fb6
Anusha <anushamahesh2506@gmail.com>
2026-04-05 19:46:40 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='healthcare_diagnostic_chatbot',
+    version='0.1.0',
+    author_name="Anusha & Ananthesh",
+    description='A healthcare diagnostic chatbot using Langchain and Pinecone',
+    author_email="ananthesh98@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
