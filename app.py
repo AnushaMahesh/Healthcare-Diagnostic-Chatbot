3abce3f00dba17f3c2d4c0c7f1bd201aec61771b
Anusha <anushamahesh2506@gmail.com>
2026-04-13 21:10:01 +0530

    web app completed

--- app.py
+++ app.py
@@ -48,11 +48,9 @@ question_answer_chain = create_stuff_documents_chain(chatModel, prompt)
 rag_chain = create_retrieval_chain(retriever, question_answer_chain)
 
 
-
 @app.route("/")
 def index():
     return render_template('chat.html')
-
 
 
 @app.route("/get", methods=["GET", "POST"])
